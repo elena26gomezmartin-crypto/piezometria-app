60b850736839d8a936899a0543806bf25da12958
elena26gomezmartin-crypto <elena26gomezmartin@gmail.com>
2026-03-24 09:48:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,24 +13,25 @@ if archivo:
     # Limpiar columnas
     df.columns = df.columns.str.strip()
 
-    st.write("Columnas detectadas:", df.columns)
-
-    # 🔑 NOMBRES REALES DE TU EXCEL
-    col_fecha = "Referencia medida"
-    col_nivel = "Referencia (m)"
+    # Nombres reales
+    col_fecha = "FECHA"
+    col_nivel = "NIVEL (m)"
     col_codigo = "CODIGO"
-    col_provincia = "MUNICIPIO"  # puedes cambiarlo luego si quieres
+    col_provincia = "PROVINCIA"
 
     # Convertir fecha
-    df[col_fecha] = pd.to_datetime(df[col_fecha])
+    df[col_fecha] = pd.to_datetime(df[col_fecha], errors="coerce")
+
+    # Quitar filas sin fecha
+    df = df.dropna(subset=[col_fecha])
 
     # FILTROS
     st.sidebar.header("Filtros")
 
-    provincia = st.sidebar.selectbox("Municipio", df[col_provincia].unique())
+    provincia = st.sidebar.selectbox("Provincia", df[col_provincia].dropna().unique())
     df = df[df[col_provincia] == provincia]
 
-    piezometro = st.sidebar.selectbox("Piezómetro", df[col_codigo].unique())
+    piezometro = st.sidebar.selectbox("Piezómetro", df[col_codigo].dropna().unique())
     df_filtrado = df[df[col_codigo] == piezometro]
 
     df_filtrado = df_filtrado.sort_values(col_fecha)
