dc00c8c59634cbb0d5ee88599ba3eaa450706513
elena26gomezmartin-crypto <elena26gomezmartin@gmail.com>
2026-03-24 09:40:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ archivo = st.file_uploader("Sube tu Excel", type=["xlsx"])
 if archivo:
     df = pd.read_excel(archivo)
 
-    df["Fecha de medida"] = pd.to_datetime(df["Fecha de medida"])
+    # Limpiar nombres de columnas
+df.columns = df.columns.str.strip()
 
+# Mostrar columnas para ver nombres reales
+st.write("Columnas:", df.columns)
+
+# Buscar columna de fecha
+col_fecha = [col for col in df.columns if "fecha" in col.lower()][0]
+
+df[col_fecha] = pd.to_datetime(df[col_fecha])
     st.subheader("Vista de datos")
     st.dataframe(df)
 
@@ -22,6 +30,6 @@ if archivo:
     df_filtrado = df_filtrado.sort_values("Fecha")
 
     st.subheader("Evolución del nivel")
-    st.line_chart(df_filtrado.set_index("Fecha de medida")["Nivel"])
+    st.line_chart(df_filtrado.set_index(col_fecha)["Nivel"])
 
     st.write("Nivel medio:", round(df_filtrado["Nivel"].mean(), 2))
