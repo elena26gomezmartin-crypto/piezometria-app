52694a045d6d5ba070c2ac6073f0190c361f4504
elena26gomezmartin-crypto <elena26gomezmartin@gmail.com>
2026-03-24 11:32:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,16 @@ if archivo:
         df.columns = df.columns.astype(str).str.strip()
 
         # 🔍 Detectar columnas automáticamente
-        col_codigo = next((c for c in df.columns if "codigo" in c.lower()), None)
-        col_municipio = next((c for c in df.columns if "municipio" in c.lower()), None)
-        col_fecha = next((c for c in df.columns if "fecha" in c.lower()), None)
-        col_nivel = next((c for c in df.columns if "nivel" in c.lower()), None)
-        col_sector = next((c for c in df.columns if "sector" in c.lower()), None)
+       columnas = [
+    "CODIGO",
+    "MUNICIPIO",
+    "FECHA",
+    "NIVEL (m)",
+    "SECTOR"
+]
 
-        columnas = [col_codigo, col_municipio, col_fecha, col_nivel, col_sector]
-
+# Filtrar solo las que existan (por seguridad)
+columnas = [col for col in columnas if col in df.columns]
         # Quitar None (por si alguna no se detecta)
         columnas = [c for c in columnas if c is not None]
 
